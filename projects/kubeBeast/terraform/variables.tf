5955234b0880733e59f647ba9cb76dc79dfcdb6e
xgueret <971xavier.gueret@gmail1.com>
2025-05-04 21:43:49 +0200

    initial

---
+++ projects/kubeBeast/terraform/variables.tf
@@ -0,0 +1,12 @@
+# Ces variables sont utilisées uniquement pour le provider
+variable "pm_api_url" {
+  type = string
+}
+
+variable "pm_api_token_id" {
+  type = string
+}
+
+variable "pm_api_token_secret" {
+  type = string
+}
